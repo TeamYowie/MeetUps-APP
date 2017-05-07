70fdaef2f8c6b90ffc80e7227489210ee1ac43b9
ArnaudovSt <telerikacademy2016@gmail.com>
2017-05-07 21:21:36 +0300

    feedback post

--- app/scripts/feedback-controller.js
+++ app/scripts/feedback-controller.js
@@ -2,9 +2,10 @@ import { Requester } from "requester";
 import { Templates } from "templates";
 import { Controller } from "controller";
 
+const STORAGE_USERNAME_KEY = "username";
+
 export class FeedbackController {
     static loadFeedback() {
-
         let feedback;
         Promise.all([Controller.isLoggedIn(), Requester.getJSON("/api/feedback")])
             .then(([isLoggedIn, data]) => {
@@ -17,7 +18,37 @@ export class FeedbackController {
             })
             .then(template => {
                 $("#content").html(template(feedback));
+                $("#feedback-error").toggleClass("hidden");
+                $("#feedback-submit").on("click", FeedbackController.postFeedback);
             }
             );
+    }
+    static postFeedback() {
+        let name = localStorage.getItem(STORAGE_USERNAME_KEY),
+            title = $("#feedback-form-title").val(),
+            message = $("#feedback-form-message").val();
+
+        //need some better validation
+        if (!name || !title || !message) {
+            return;
+        }
+
+        Requester.postJSON("/api/feedback", {
+            name,
+            title,
+            message
+        })
+            .then(res => {
+                if (res.status === 201) {
+                    FeedbackController.loadFeedback();
+                }
+            })
+            .catch(templateError => {
+                let errorElement = $("#feedback-error");
+                if (templateError.status === 422) {
+                    errorElement.text(templateError.responseText);
+                    Controller.errorPopup(errorElement);
+                }
+            });
     }
 }
